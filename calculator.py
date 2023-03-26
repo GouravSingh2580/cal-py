1bef347c9ab8fce0266451dfc1defad611cb4caa
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-03-26 12:41:24 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,14 @@
-# calculator.py
-
-def calculator():
-    expression = input("Enter a mathematical expression: ")
+def calculate(expression):
     try:
         result = eval(expression)
+        return result
+    except Exception as e:
+        raise Exception("Invalid expression: {}".format(expression))
+
+if __name__ == '__main__':
+    expression = input("Enter a mathematical expression: ")
+    try:
+        result = calculate(expression)
         print("Result: ", result)
     except Exception as e:
         print("Error: ", e)
